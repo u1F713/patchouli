70b7c4e756857036e74f9860ad573e30b89ce054
Nyarlathotep <anhedonia@skiff.com>
2024-07-01 03:40:37 -0400

    feat(rss): item enclosure

--- src/pages/chronicles/rss.xml.ts
+++ src/pages/chronicles/rss.xml.ts
@@ -1,8 +1,23 @@
 import type { APIContext } from 'astro'
 import { getCollection } from 'astro:content'
+import { Cloudinary } from '@cloudinary/url-gen'
+import { getImage } from 'astro:assets'
 import rss from '@astrojs/rss'
 import sanitizeHtml from 'sanitize-html'
 import MarkdownIt from 'markdown-it'
+
+const cld = new Cloudinary({
+  cloud: { cloudName: import.meta.env.CLOUDINARY_CLOUD_NAME }
+})
+
+async function getCloudinaryImage(url: string) {
+  const { src } = await getImage({
+    src: cld.image(url).createCloudinaryURL(),
+    inferSize: true
+  })
+
+  return src
+}
 
 const parser = new MarkdownIt()
 
@@ -14,15 +29,25 @@ export async function GET(context: APIContext) {
     description: 'Good for health, bad for...',
     site: `${context.site}`,
 
-    items: chronicles.map(chronicle => ({
-      title: chronicle.data.title,
-      pubDate: chronicle.data.pubDate,
-      description: chronicle.data.description,
-      link: `${import.meta.env.BASE_URL}/chronicles/${chronicle.slug}/`,
+    items: await Promise.all(
+      chronicles.map(async chronicle => ({
+        title: chronicle.data.title,
+        pubDate: chronicle.data.pubDate,
+        description: chronicle.data.description,
+        link: `${import.meta.env.BASE_URL}/chronicles/${chronicle.slug}/`,
 
-      content: sanitizeHtml(parser.render(chronicle.body))
-    })),
+        enclosure: chronicle.data.image
+          ? {
+              type: 'image/jpeg',
+              url: await getCloudinaryImage(chronicle.data.image),
+              length: 1
+            }
+          : undefined,
 
-    customData: `<language>en-us</language>`
+        content: sanitizeHtml(parser.render(chronicle.body))
+      }))
+    ),
+
+    customData: '<language>en-us</language>'
   })
 }
