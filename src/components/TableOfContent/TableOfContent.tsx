67af9931d0a7c0eb261a2bdb99392d9271f7119b
Nyarlathotep <anhedonia@skiff.com>
2024-07-11 19:57:18 -0400

    feat: refactored TOC component

--- src/components/TableOfContent/TableOfContent.tsx
+++ src/components/TableOfContent/TableOfContent.tsx
@@ -1,5 +1,13 @@
 import { MarkdownHeading } from 'astro'
-import { Component, createSignal, onCleanup, onMount } from 'solid-js'
+import {
+  Component,
+  createSelector,
+  createSignal,
+  For,
+  onCleanup,
+  onMount
+} from 'solid-js'
+import styles from './TableOfContent.module.css'
 
 interface TOCProps {
   contentDOMId: string
@@ -15,6 +23,7 @@ function getElement(container: Document | HTMLElement, elementId: string) {
 
 const TableOfContent: Component<TOCProps> = props => {
   const [activeHeading, setActiveHeading] = createSignal<string>()
+  const isActive = createSelector(activeHeading)
 
   onMount(() => {
     const containerTarget = getElement(document, props.contentDOMId)
@@ -35,7 +44,18 @@ const TableOfContent: Component<TOCProps> = props => {
   })
 
   return (
-    <div>{props.headings.find(h => h.slug === activeHeading())?.text}</div>
+    <aside>
+      <h3 class={styles.heading}>In this article</h3>
+      <ul class={styles.TableOfContent}>
+        <For each={props.headings}>
+          {({ slug, text }) => (
+            <li data-current={isActive(slug)}>
+              <a href={`#${slug}`}>{text}</a>
+            </li>
+          )}
+        </For>
+      </ul>
+    </aside>
   )
 }
 
